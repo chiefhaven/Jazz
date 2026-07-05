d419b52271a70dd5f773bbc512e5eebb2ed42b30
chiefhaven <chikumbahappy@gmail.com>
2026-07-06 00:36:38 +0200

    Update SaleCompleted.php

--- Modules/EIS/Events/SaleCompleted.php
+++ Modules/EIS/Events/SaleCompleted.php
@@ -3,10 +3,14 @@
 namespace Modules\EIS\Events;
 
 use App\Transaction;
+use AWS\CRT\Log;
+use Illuminate\Support\Facades\Log as FacadesLog;
 
 class SaleCompleted
 {
     public function __construct(
         public Transaction $transaction
-    ) {}
+    ) {
+        FacadesLog::info('SaleCompleted event created for transaction ID: ' . $transaction->id);
+    }
 }
