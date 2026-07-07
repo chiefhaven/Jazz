aaae364aa34026b36fcb395a9482dfd9f58471c1
chiefhaven <chikumbahappy@gmail.com>
2026-07-07 23:42:04 +0200

    Update TaxMappingService.php

--- Modules/EIS/Services/Tax/TaxMappingService.php
+++ Modules/EIS/Services/Tax/TaxMappingService.php
@@ -7,15 +7,15 @@ use Modules\EIS\Models\EisTaxMapping;
 class TaxMappingService
 {
     public function resolve(int $businessId, ?int $taxId): string
-    {
+    {   
         if (!$taxId) {
-            return 'EXEMPT';
+            return 'E';
         }
 
         $map = EisTaxMapping::where('business_id', $businessId)
             ->where('tax_id', $taxId)
             ->first();
 
-        return $map?->eis_tax_rate_id ?? 'EXEMPT';
+        return $map?->eis_tax_rate_id ?? 'E';
     }
 }
