901263b16f34cb33f1afa34067c700954ae0fca6
chiefhaven <chikumbahappy@gmail.com>
2026-07-08 23:56:09 +0200

    Add soft deletes to EIS Product Map
    Add soft delete functionality to the EisProductMap model and eis_product_maps table. This allows product map records to be soft-deleted rather than permanently removed, enabling data recovery and maintaining referential integrity.

--- Modules/EIS/Models/EisProductMap.php
+++ Modules/EIS/Models/EisProductMap.php
@@ -3,9 +3,12 @@
 namespace Modules\EIS\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EisProductMap extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'business_id',
         'product_id',
