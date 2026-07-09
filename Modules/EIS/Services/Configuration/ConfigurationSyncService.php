e10109cf6e87713e6fe4ac4901b08100d6dd8744
chiefhaven <chikumbahappy@gmail.com>
2026-07-09 02:21:26 +0200

    Update ConfigurationSyncService.php

--- Modules/EIS/Services/Configuration/ConfigurationSyncService.php
+++ Modules/EIS/Services/Configuration/ConfigurationSyncService.php
@@ -2,85 +2,117 @@
 
 namespace Modules\EIS\Services\Configuration;
 
-
 use Modules\EIS\Models\EisConfiguration;
 use Modules\EIS\Models\EisTaxRate;
+
 class ConfigurationSyncService
 {
-
     public function __construct(
-        protected EisConfiguration $client
-    ){}
+        protected EisConfigurationClient $client
+    ) {
+    }
+
 
     public function sync(
-    int $businessId,
-    string $token
-) {
+        int $businessId,
+        string $token
+    ) {
 
-    $response = $this->client->latest($token);
+        $response = $this->client->latest($token);
 
-    if (!is_array($response)) {
-        throw new \Exception(
-            'Invalid EIS response format: ' . gettype($response)
+        if (!is_object($response)) {
+            throw new \Exception(
+                'Invalid EIS response format: ' . gettype($response)
+            );
+        }
+
+
+        if (($response->statusCode ?? 1) !== 0) {
+
+            throw new \Exception(
+                $response->remark ?? 'EIS configuration sync failed'
+            );
+
+        }
+
+
+        $data = $response->data;
+
+
+        $configuration = EisConfiguration::updateOrCreate(
+            [
+                'business_id' => $businessId
+            ],
+            [
+                'global_version' => 
+                    $data->globalConfiguration->versionNo ?? null,
+
+                'terminal_version' => 
+                    $data->terminalConfiguration->versionNo ?? null,
+
+                'taxpayer_version' => 
+                    $data->taxpayerConfiguration->versionNo ?? null,
+
+                'tin' => 
+                    $data->taxpayerConfiguration->tin ?? null,
+
+                'is_vat_registered' => 
+                    $data->taxpayerConfiguration->isVATRegistered ?? false,
+
+                'tax_office_code' => 
+                    $data->taxpayerConfiguration->taxOfficeCode ?? null,
+
+                'raw_response' => json_encode($data),
+
+                'last_synced_at' => now()
+            ]
         );
+
+
+        app(TaxConfigurationService::class)
+            ->sync(
+                $configuration,
+                $data
+            );
+
+
+        app(TerminalConfigurationService::class)
+            ->sync(
+                $configuration,
+                $data
+            );
+
+
+        return $configuration;
+
     }
 
-    if (($response['statusCode'] ?? 1) !== 0) {
-        throw new \Exception(
-            $response['remark'] ?? 'EIS configuration sync failed'
-        );
-    }
 
-    $data = $response['data'] ?? [];
-
-    $configuration = EisConfiguration::updateOrCreate(
-        [
-            'business_id' => $businessId
-        ],
-        [
-            'global_version' => $data['globalConfiguration']['versionNo'] ?? null,
-            'terminal_version' => $data['terminalConfiguration']['versionNo'] ?? null,
-            'taxpayer_version' => $data['taxpayerConfiguration']['versionNo'] ?? null,
-            'tin' => $data['taxpayerConfiguration']['tin'] ?? null,
-            'is_vat_registered' => $data['taxpayerConfiguration']['isVATRegistered'] ?? false,
-            'tax_office_code' => $data['taxpayerConfiguration']['taxOfficeCode'] ?? null,
-            'raw_response' => $data,
-            'last_synced_at' => now()
-        ]
-    );
-
-    app(TaxConfigurationService::class)
-        ->sync($configuration, $data);
-
-    app(TerminalConfigurationService::class)
-        ->sync($configuration, $data);
-
-    return $configuration;
-}
 
     private function syncTaxRates(
         $configuration,
         $config
-    ){
+    ) {
 
-        $rates =
-            $config['globalConfiguration']['taxrates']
-            ?? [];
+        $rates = 
+            $config->globalConfiguration->taxrates ?? [];
 
 
-        foreach($rates as $rate){
+        foreach ($rates as $rate) {
+
             EisTaxRate::updateOrCreate(
                 [
                     'configuration_id' => $configuration->id,
-                    'eis_tax_rate_id' => $rate['id']
+                    'eis_tax_rate_id' => $rate->id
                 ],
                 [
-                    'name' => $rate['name'],
-                    'charge_mode' => $rate['chargeMode'],
-                    'rate' => $rate['rate'],
-                    'ordinal' => $rate['ordinal']
+                    'name' => $rate->name,
+                    'charge_mode' => $rate->chargeMode,
+                    'rate' => $rate->rate,
+                    'ordinal' => $rate->ordinal
                 ]
             );
+
         }
 
     }
