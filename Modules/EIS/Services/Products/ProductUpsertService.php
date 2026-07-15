8be1ea1c28fdd68afc0280475a153a11e5554743
chiefhaven <chikumbahappy@gmail.com>
2026-07-16 01:20:28 +0200

    Update ProductUpsertService.php

--- Modules/EIS/Services/Products/ProductUpsertService.php
+++ Modules/EIS/Services/Products/ProductUpsertService.php
@@ -356,10 +356,6 @@ class ProductUpsertService {
             [
                 'product_id' => $product->id,
                 'location_id' => $locationId,
-            ],
-            [
-                'created_at' => now(),
-                'updated_at' => now(),
             ]
         );
 
