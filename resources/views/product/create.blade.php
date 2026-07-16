78f8f19cc925dc95530453db0a90d54e156b85c3
chiefhaven <chikumbahappy@gmail.com>
2026-07-17 01:13:56 +0200

    Update create.blade.php

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -149,7 +149,7 @@
         <div class="col-sm-8">
             <div class="form-group">
                 {!! Form::label('product_description', __('lang_v1.product_description') . ':') !!}
-                {!! Form::textarea('product_description', !empty($duplicate_product->product_description) ? $duplicate_product->product_description : null, ['class' => 'form-control']); !!}
+                {!! Form::text('product_description', !empty($duplicate_product->product_description) ? $duplicate_product->product_description : null, ['class' => 'form-control']); !!}
             </div>
         </div>
         <div class="col-sm-4">
